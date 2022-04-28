a0d22b6786a301e6b7a3d4d8648e47388006ff75
Csandrade1 <andrade.cynthia07@gmail.com>
2022-04-27 23:50:43 -0700

    almost done!

---
+++ advance-form-management/src/component/Form.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  first_name: yup.string().required("first name is required"),
+  last_name: yup.string().required("last name is required"),
+  email: yup.string().required("email is required"),
+  password: yup.string().required("password is required"),
+  agree: yup.boolean().oneOf([true], "you understand the terms of service"),
+});
+
+export default function Form() {
+  const [form, setForm] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    agree: false,
+  });
+
+  const [errors, setErrors] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    agree: "",
+  });
+
+  const [disabled, setDisabled] = useState(true);
+
+  const setFormErrors = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => setErrors({ ...errors, [name]: "" }))
+      .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
+  };
+
+  const change = (event) => {
+    const { checked, value, name, type } = event.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    setFormErrors(name, valueToUse);
+    setForm({ ...form, [name]: valueToUse });
+  };
+
+  useEffect(() => {
+    schema.isValid(form).then((valid) => setDisabled(!valid));
+  }, [form]);
+
+  return (
+    <div className="Form">
+      <div style={{ color: "red" }}>
+        <div>{errors.first_name}</div>
+        <div>{errors.last_name}</div>
+        <div>{errors.email}</div>
+        <div>{errors.password}</div>
+        <div>{errors.agree}</div>
+      </div>
+      <form>
+        <label>
+          First Name
+          <input
+            onChange={change}
+            value={form.first_name}
+            name="first_name"
+            type="text"
+          />
+        </label>
+
+        <label>
+          Last Name
+          <input
+            onChange={change}
+            value={form.last_name}
+            name="last_name"
+            type="text"
+          />
+        </label>
+
+        <label>
+          Email
+          <input
+            onChange={change}
+            value={form.email}
+            name="email"
+            type="text"
+          />
+        </label>
+
+        <label>
+          Password
+          <input
+            onChange={change}
+            value={form.password}
+            name="password"
+            type="password"
+          />
+        </label>
+
+        <label>
+          Terms of Service
+          <input
+            onChange={change}
+            checked={form.agree}
+            name="agree"
+            type="checkbox"
+          />
+        </label>
+
+        <button disabled={disabled} className="sumbit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
